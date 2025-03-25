e08f2f1d78f5e00958dc401fa3d5a7a0314ca65b
Joakim Lundborg <joakim@lind.surf>
2025-03-25 10:30:47 +0100

    Add brackets and partial keywords

--- languages/lispbm/highlights.scm
+++ languages/lispbm/highlights.scm
@@ -1,9 +1,13 @@
 (symbol) @variable
-(application name: (symbol) @function)
+(application function: (symbol) @function)
+(application arg: (symbol) @function.argument)
 (number) @number
+(directive) @preproc
 (string) @string
 ["(" ")" "{" "}"] @punctuation.bracket
+["defun" "def" "define"] @keyword
 (comment) @comment
 (special) @keyword
-(function_definition name: (symbol) @keyword)
+(function_definition name: (symbol) @function.name)
 (arglist (symbol) @variable.special )
+(ERROR) @warning
